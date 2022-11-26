1ed32a33660ec721a124e13d4def6d2012682cfc
MACARENASTORE <oclusiva9204@gmail.com>
2022-11-26 10:00:12 -0500

    prueba4

--- config/database.js
+++ config/database.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
 
-const host ="127.0.0.1";
-const port = "27017";
-const db = "dbpoporo";
+// const host ="127.0.0.1";
+// const port = "27017";
+// const db = "dbpoporo";
 
-exports.mongoConnect = () => {
+exports.mongoConnect = async () => {
     const mongoStringConnection = "mongodb+srv://admin:admin@cluster0.ogxdz8m.mongodb.net/dbpoporo"
-  
+    try {
+      await mongoose.connect(mongoStringConnection)
+      console.log("db online")
+    } catch (error) {
+      console.log(error);
+      throw new Error("Couldn't connect to Mongo")
+
+    }
+
+
   //  const mongoStringConnection = `mongodb://${host}:${port}/${db}`; 
     
-    mongoose.connect(mongoStringConnection);
-    mongoose.Promise = global. Promise;
-    const dbConnection = mongoose.connection;
-    dbConnection.on("error", console.error.bind(console, "Mongodb conection error"))
+    // mongoose.connect(mongoStringConnection);
+    // mongoose.Promise = global. Promise;
+    // const dbConnection = mongoose.connection;
+    // dbConnection.on("error", console.error.bind(console, "Mongodb conection error"))
 
 }
